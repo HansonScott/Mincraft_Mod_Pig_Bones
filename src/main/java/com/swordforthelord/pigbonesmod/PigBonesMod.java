00d538ea8de9cbbb2f9c6911c3d49d2e6844f368
Scott Hanson <shanson77@aol.com>
2022-04-25 17:23:03 -0600

    Initial commit

---
+++ src/main/java/com/swordforthelord/pigbonesmod/PigBonesMod.java
@@ -0,0 +1,9 @@
+package com.swordforthelord.pigbonesmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(PigBonesMod.MOD_ID)
+
+public class PigBonesMod {
+    public static final String MOD_ID = "pigbonesmod";
+}
